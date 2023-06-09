73306402ba923c9fd7c15b96e368e5228933718f
seemyown <semyon.albeev@gmail.com>
2023-06-09 14:16:05 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,22 @@
+import datetime as dt
+
+from passlib.context import CryptContext
+
+from application.salary import calculate_salary
+from application.db.people import get_employees
+
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+class Hasher:
+
+    @staticmethod
+    def hash_pass(passowrd):
+        return pwd_context.hash(passowrd)
+
+if __name__ == '__main__':
+    print(f"Current date: {dt.datetime.now()}")
+    pwd = input("Введите пароль: ")
+    print(f"Ваш захэшированный пароль {Hasher.hash_pass(pwd)}") 
+    get_employees()
+    calculate_salary()
